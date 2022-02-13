8e15780ebfcc3755250285c5149446061466e41a
Cas <caskeizers@gmail.com>
2022-02-13 21:22:17 +0100

    corrected flow calc

--- src/calcs.js
+++ src/calcs.js
@@ -14,25 +14,25 @@ const calcArea = (diameter) => {
 
 const calcFlow = (diameter, vel, rho) => {
     const area = calcArea(convertInchToMeter(diameter));
-    const flow = area * vel * rho;
+    const flow = area * vel * rho * 3.6;
     return Math.round(flow * 100) / 100;
 }
 
 const calcDiameter = (flow, vel, rho) => {
-    const area = flow/3.6 / (vel * rho);
+    const area = flow / 3.6 / (vel * rho);
     const diameter = convertMeterToInch(((4 * area) / pi) ** 0.5);
     return Math.round(diameter * 100) / 100;
 }
 
 const calcVel = (flow, diameter, rho) => {
     const area = calcArea(convertInchToMeter(diameter));
-    const vel = flow/3.6 / (area * rho);
+    const vel = flow / 3.6 / (area * rho);
     return Math.round(vel * 100) / 100;
 }
 
 const calcRho = (flow, diameter, vel) => {
     const area = calcArea(convertInchToMeter(diameter));
-    const rho =  flow/3.6 / (area * vel);
+    const rho =  flow / 3.6 / (area * vel);
     return Math.round(rho * 100) / 100;
 }
 
